ddda837a706dd4c0b145c8b47b2c90f4bed6a73d
Akiva Majowka <akivamaj@gmail.com>
2022-08-29 21:45:09 +0300

    ..

--- script.js
+++ script.js
@@ -1,73 +1,67 @@
 let myLibrary = [
-    {
-        name: 'Harry Potter',
-        author: 'J.K Rolling',
-        pages: 345,
-        didRead: true
-    },
-    {
-        name: 'Harry Potter',
-        author: 'J.K Rolling',
-        pages: 345,
-        didRead: true
-    },
-    {
-        name: 'Harry Potter',
-        author: 'J.K Rolling',
-        pages: 345,
-        didRead: true
-    },
-    {
-        name: 'Harry Potter',
-        author: 'J.K Rolling',
-        pages: 345,
-        didRead: true
-    }
+  {
+    name: "Harry Potter",
+    author: "J.K Rolling",
+    pages: 345,
+    didRead: true,
+  },
+  {
+    name: "Harry Potter",
+    author: "J.K Rolling",
+    pages: 345,
+    didRead: true,
+  },
+  {
+    name: "Harry Potter",
+    author: "J.K Rolling",
+    pages: 345,
+    didRead: true,
+  },
+  {
+    name: "Harry Potter",
+    author: "J.K Rolling",
+    pages: 345,
+    didRead: true,
+  },
 ];
 
 function renderLibrary() {
-    let grid = document.querySelector('.books-grid')
+  let grid = document.querySelector(".books-grid");
 
-    myLibrary.forEach(item => {
-        let bookCardWrap = document.createElement("div")
-        bookCardWrap.classList.add("book-card-wraper");
+  myLibrary.forEach((item) => {
+    let bookCardWrap = document.createElement("div");
+    bookCardWrap.classList.add("book-card-wraper");
 
-        let bookName = document.createElement("h1")
-        bookName.innerText = item.name;
+    let bookName = document.createElement("h1");
+    bookName.innerText = item.name;
 
-        let bookAuthor = document.createElement("p")
-        bookAuthor.innerText = `By: ${item.author}`;
+    let bookAuthor = document.createElement("p");
+    bookAuthor.innerText = `By: ${item.author}`;
 
-        let bookpages = document.createElement("p")
-        bookpages.innerText = `${item.pages} Pages`;
+    let bookpages = document.createElement("p");
+    bookpages.innerText = `${item.pages} Pages`;
 
-        bookCardWrap.append(bookName, bookAuthor, bookpages);
-        grid.append(bookCardWrap)
-    });
+    bookCardWrap.append(bookName, bookAuthor, bookpages);
+    grid.append(bookCardWrap);
+  });
 }
 
 // Book constructor
 function Book(name, author, pages) {
-    this.name = name,
-    this.author = author,
-    this.pages = pages
+  (this.name = name), (this.author = author), (this.pages = pages);
 }
 
 function addBookToLibrary() {
-    myLibrary[0].name = 'akiva'
-    renderLibrary()
-
   let nameInput = document.querySelector("#book-name-input").value;
   let authorInput = document.querySelector("#author-name-input").value;
   let pagesInput = document.querySelector("#num-pages").value;
 
-  newBook = new book(nameInput, authorInput, pagesInput)
-  myLibrary.push(newBook)
-  renderLibrary()
+  let newBook = new book(nameInput, authorInput, pagesInput);
+  myLibrary.push(newBook);
+  renderLibrary();
 }
 
-
-renderLibrary()
+renderLibrary();
 
 let addButton = document.querySelector("#add-button");
-addButton.addEventListener('click', addBookToLibrary)
+addButton.addEventListener("click", addBookToLibrary);
